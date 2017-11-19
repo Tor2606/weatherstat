8513d71cc5b59c6c3290d6eda7a684d698d27c88
Ihor Slabuhin <ihor.slabuhin@audienzz.ch>
2017-11-19 13:28:21 +0200

    master First commit

---
+++ src/main/java/com/weatherstat/weatherstat/WeatherstatApplication.java
@@ -0,0 +1,12 @@
+package com.weatherstat.weatherstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherstatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/weatherstat/weatherstat/WeatherstatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weatherstat.weatherstat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherstatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
